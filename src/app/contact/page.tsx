1b8ab53c7e9bc4a7dbe2237b9ed0065312f5c8fa
jay6o <richforeverrr6@proton.me>
2025-03-19 22:12:10 -0400

    contact progress

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,24 +1,31 @@
-import Nav from "../../components/nav";
-import Footer from "../../components/footer";
+import Nav from "@/components/nav";
+import { Phone } from "lucide-react";
+import { Mail } from "lucide-react";
+import Footer from "@/components/footer";
+
 export default function Contact() {
   return (
     <>
       <Nav />
-      <div className="min-h-fit h-[calc(100vh-6rem)] flex items-center">
-        <div className="w-full lg:w-1/2 border-r border-r-2 border-black items-left">
-          <div className="h-1/2 flex flex-col justify-around items-center">
-            <h1 className="text-4xl text-left font-semibold">{`Let's make you greater!`}</h1>
-            <p className="text-left text-2xl w-2/3 py-8">Get a free estimation & consultation for your upcoming software project, no strings attached.</p>
-            <ul className="">
-              <li className="text-4xl font-bold py-8">E-Mail</li>
-              <li className="text-4xl font-bold">Instagram</li>
-            </ul>
+      <div className="min-h-fit h-[calc(100vh-10rem)] lg:h-[calc(100vh)] pt-[5rem] lg:pt-[6rem] flex flex-col lg:flex-row justify-around lg:justify-between items-center w-full bg-[radial-gradient(ellipse_at_30%_100%,_#c4c4ff_0%,_#f2f2f2_60%)]">
+        <div className="w-full lg:w-1/2 pt-[4rem] md:pt-[2rem] lg:pt-[6rem] h-1/2 flex flex-col justify-center items-center bg-transparent">
+          <div className="w-2/3 h-full bg-transparent">
+            <h2 className="text-5xl font-medium py-6 bg-transparent">Ready to build?</h2>
+            <p className="text-2xl bg-transparent h-fit">Reach out to us about your project and we&apos;ll give you a <b>free</b> consultation!</p>
           </div>
         </div>
-        <div className="hidden lg:w-1/2">
+        <div className="w-2/3 lg:w-1/2 h-full max-h-[48rem] flex flex-col justify-start lg:justify-center items-center lg:pt-[5rem] bg-transparent">
+          <div className="h-1/3 w-full md:w-[32rem] lg:w-[24rem] border border-black/40 rounded-lg flex flex-col justify-around items-center py-2">
+            <div className="h-[40%] w-[90%] secondary rounded-lg border border-black/25 flex items-center px-8">
+              <Phone size={32} />
+            </div>
+            <div className="h-[40%] w-[90%] secondary rounded-lg border border-black/25 flex items-center px-8">
+              <Mail size={32} />
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
     </>
   );
-};
+}
