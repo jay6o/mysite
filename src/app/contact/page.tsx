9533e965e461a91797f2ac95531b654b6b58af2d
jay6o <richforeverrr6@proton.me>
2025-03-20 11:33:48 -0400

    contact page + ui changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,21 +7,20 @@ export default function Contact() {
   return (
     <>
       <Nav />
-      <div className="min-h-fit h-[calc(100vh-10rem)] lg:h-[calc(100vh)] pt-[5rem] lg:pt-[6rem] flex flex-col lg:flex-row justify-around lg:justify-between items-center w-full bg-[radial-gradient(ellipse_at_30%_100%,_#c4c4ff_0%,_#f2f2f2_60%)]">
-        <div className="w-full lg:w-1/2 pt-[4rem] md:pt-[2rem] lg:pt-[6rem] h-1/2 flex flex-col justify-center items-center bg-transparent">
-          <div className="w-2/3 h-full bg-transparent">
-            <h2 className="text-5xl font-medium py-6 bg-transparent">Ready to build?</h2>
-            <p className="text-2xl bg-transparent h-fit">Reach out to us about your project and we&apos;ll give you a <b>free</b> consultation!</p>
-          </div>
+      <div className="min-h-fit h-[calc(100vh)] py-[2.5rem] lg:pt-[6rem] flex flex-col lg:flex-row justify-center lg:justify-around items-center w-full bg-[radial-gradient(ellipse_at_30%_100%,_#c4c4ff_0%,_#f2f2f2_60%)] w-screen">
+        <div className="h-1/2 w-2/3 lg:w-1/3 flex flex-col justify-center items-start bg-transparent">
+          <h2 className="text-[2rem] md:text-[2.8rem] lg:text-[3.5rem] font-semibold py-4">Ready to build?</h2>
+          <p className="text-[1.25rem] md:text-[2rem] lg:text-[2rem]">Reach out to us about your project and we&apos;ll give you a <b>free</b> consultation</p>
         </div>
-        <div className="w-2/3 lg:w-1/2 h-full max-h-[48rem] flex flex-col justify-start lg:justify-center items-center lg:pt-[5rem] bg-transparent">
-          <div className="h-1/3 w-full md:w-[32rem] lg:w-[24rem] border border-black/40 rounded-lg flex flex-col justify-around items-center py-2">
-            <div className="h-[40%] w-[90%] secondary rounded-lg border border-black/25 flex items-center px-8">
-              <Phone size={32} />
-            </div>
-            <div className="h-[40%] w-[90%] secondary rounded-lg border border-black/25 flex items-center px-8">
-              <Mail size={32} />
-            </div>
+
+        <div className="w-2/3 lg:w-1/4 h-1/3 lg:h-[30%] max-h-[48rem] flex flex-col justify-around border rounded-xl border-black/30 items-center shadow-2xl">
+          <div className="secondary h-[40%] w-[90%] border border-black/15 rounded-xl shadow-lg flex items-center px-[5%]">
+            <Phone className="h-[2rem] w-[2rem] md:h-[2.8rem] md:w-[4rem]" />
+            <span className="px-4 text-[1rem] md:text-[1.5rem]">804-xxx-xxxx</span>
+          </div>
+          <div className="secondary h-[40%] w-[90%] border border-black/15 rounded-xl shadow-lg flex items-center px-[5%]">
+            <Mail className="h-[2rem] w-[2rem] md:h-[2.8rem] md:w-[4rem]" />
+            <span className="px-4 text-[1rem] md:text-[1.5rem] lg:text-[1rem]">email@email.com</span>
           </div>
         </div>
       </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        custom: "secondary border border-black border-2 text-black shadow transition duration-500 hover:bg-black hover:border-black hover:text-white"
+        custom: "secondary border border-black border-2 text-black shadow transition duration-500 hover:bg-black hover:border-black hover:text-white shadow-2xl"
       },
       size: {
         default: "h-9 px-4 py-2",
