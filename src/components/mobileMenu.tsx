08574eff67ffb4f9abf8ea517762dd04fa9405b4
jay6o <richforeverrr6@proton.me>
2025-03-21 11:57:08 -0400

    fixed mobile menu staying open on same page hrefs

--- src/components/mobileMenu.tsx
+++ src/components/mobileMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { X } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 interface MobileMenuProps {
   isOpen: boolean;
@@ -8,6 +9,10 @@ interface MobileMenuProps {
 }
 
 export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
+  const path = usePathname();
+  const closeMenuSamePage = (to: string, as: string) => {
+    to === as ? onClose() : null;
+  }
   return (
     <div
       className={`fixed top-0 right-0 h-screen w-[90vw] bg-[#f2f2f2] shadow-lg z-40 border-l border-black transform transition-transform duration-700 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full border-l'
@@ -17,11 +22,11 @@ export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
         <button onClick={onClose} className="self-end mb-8">
           <X size={42} />
         </button>
-        <Link href="/services" className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Services</p></Link>
-        <Link href="/about" className="text-xl my-2 "><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">About Us</p></Link>
-        <Link href="/contact" className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Contact</p></Link>
-        <Link href="/privacy" className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Privacy Policy</p></Link>
-        <Link href="/terms" className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Terms of Services</p></Link>
+        <Link href="/#services" onClick={() => closeMenuSamePage("/", path)} className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Services</p></Link>
+        <Link href="/about" onClick={() => closeMenuSamePage("/about", path)} className="text-xl my-2 "><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">About Us</p></Link>
+        <Link href="/contact" onClick={() => closeMenuSamePage("/contact", path)} className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Contact</p></Link>
+        <Link href="/privacy" onClick={() => closeMenuSamePage("/privacy", path)} className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Privacy Policy</p></Link>
+        <Link href="/terms" onClick={() => closeMenuSamePage("/terms", path)} className="text-xl my-2"><p className="text-3xl py-2 transition-all duration:300 ease-in-out hover:underline">Terms of Services</p></Link>
       </nav>
     </div>
   );
